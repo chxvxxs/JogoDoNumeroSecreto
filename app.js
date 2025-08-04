4a4fcf63139a6c472fe077d07ca853ccefede314
chxvxxs <mcldhy@gmail.com>
2025-08-04 17:05:35 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-let numeroSecreto = gerarNumeroAleatorio;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
+
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let msgTentativas = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', msgTentativas);
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else{
+                exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+
+        tentativas ++;
+        limparCampo();
+    }
 }
+
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
 }
